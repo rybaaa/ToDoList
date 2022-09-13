2f49bf709dbaa3574bc0d1dbeae7d7250166cf43
rybaaa <bolshayaryba22@gmail.com>
2022-09-13 22:04:09 +0300

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import TodoList from "./TodoList";
-import todoList from "./TodoList";
+
 
 function App() {
     const todolistTitle = 'What to learn'
